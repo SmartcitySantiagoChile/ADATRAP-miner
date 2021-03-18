6741959df99c446f40f3fdd2b035e281e720fe74
Bastianleaf <bastian.martinez@ing.uchile.cl>
2021-03-18 18:02:04 -0300

    Add Cloudwatch logs to windows_miner.py

--- windows_miner.py
+++ windows_miner.py
@@ -6,7 +6,10 @@ import sys
 
 from decouple import config
 
+from adatrapMiner import aws
+
 logger = logging.getLogger(__name__)
+general_log_stream = config("GENERAL_LOG_STREAM")
 
 
 def main(argv):
@@ -18,15 +21,24 @@ def main(argv):
 
     # Arguments and description
     parser = argparse.ArgumentParser(description="Script to execute ADATRAP")
-    parser.add_argument("date", help="date to execute adatrap")
-
+    parser.add_argument("date", help="Date to execute adatrap")
+    parser.add_argument("id", help="EC2 Instance ID")
     parser.add_argument(
         "-v", "--verbose", help="increase output verbosity", action="store_true"
     )
     args = parser.parse_args(argv[1:])
     date = args.date
+    instance_id = args.id
     path = config("ADATRAP_PATH")
-    logger.info(f"{date} {path}")
+
+    # Initial Log
+    session = aws.AWSSession()
+
+    message = "Instancia inicializada."
+    logger.info(message)
+    session.send_log_event(instance_id, message)
+
+    # Run ADATRAP
     subprocess.run(
         [os.path.join(path, "pvmts_dummy.exe"), os.path.join(path, f"{date}.par")],
         shell=True,
